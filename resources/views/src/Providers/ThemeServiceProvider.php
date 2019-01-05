b13fd4f0343f3a8d6052e459d3852a17cfba530d
Ridvan Koc <ridvankoc>
2019-01-05 08:50:10 +0100

    Initial copy of plenty-tc24

---
+++ resources/views/src/Providers/ThemeServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Theme\Providers;
+
+use IO\Helper\TemplateContainer;
+use IO\Helper\ComponentContainer;
+use Plenty\Plugin\Events\Dispatcher;
+use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+  const PRIORITY = 0;
+  /**
+  * Register the service provider.
+  */
+  public function register()
+  {
+  }
+  /**
+  * Boot a template for the basket that will be displayed in the template plugin instead of the original basket.
+  */
+  public function boot(Twig $twig, Dispatcher $eventDispatcher)
+  {
+    $eventDispatcher->listen('IO.Component.Import', function (ComponentContainer $container)
+    {
+      if ($container->getOriginComponentTemplate()=='Ceres::Item.Components.SingleItem')
+      {
+        $container->setNewComponentTemplate('Theme::content.SingleItem');
+      }
+      
+      if ($container->getOriginComponentTemplate()=='Ceres::Checkout.Components.ShippingProfileSelect')
+      {
+        $container->setNewComponentTemplate('Theme::content.ShippingProfileSelect');
+      }
+    }, self::PRIORITY);
+  }
+}
